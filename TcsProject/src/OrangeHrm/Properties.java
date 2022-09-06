ff1438d4532f45f045fa8bf995dbc983bc2976cd
Hp <Hp@DESKTOP-0QRJC3G>
2022-09-06 16:38:01 +0530

    First commit

---
+++ TcsProject/src/OrangeHrm/Properties.java
@@ -0,0 +1,13 @@
+package OrangeHrm;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Properties {
+	 public void Test(String key) throws Exception, Throwable {
+	    	String value = "";
+	    	Properties prop = new Properties();
+	    	File file = new File("./TcsProject/src/OrangeHrm/properties");
+	    	file.exists();
+	 }
+}
